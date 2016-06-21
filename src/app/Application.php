7fa1d6dca916fb536e658c520cd254ae93c59897
Nathan McBride <nathan@brideo.co.uk>
2016-06-21 21:52:20 +0100

    Allow composer global installs

---
+++ src/app/Application.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Brideo\Magento2Scaffolding;
+
+use Brideo\Magento2Scaffolding\Command\Frontend\TemplateBlockCommand as FrontendTemplateBlock;
+use Brideo\Magento2Scaffolding\Command\Adminhtml\TemplateBlockCommand as AdminTemplateBlock;
+use Brideo\Magento2Scaffolding\Command\GenerateCommand;
+use Brideo\Magento2Scaffolding\Command\Adminhtml\RouteCommand as AdminRoute;
+use Brideo\Magento2Scaffolding\Command\Frontend\RouteCommand as FrontendRoute;
+use Symfony\Component\Console\Application as SymfonyApplication;
+
+class Application
+{
+
+    /**
+     * @var SymfonyApplication
+     */
+    protected $application;
+
+    /**
+     * Application constructor.
+     *
+     * @param SymfonyApplication $application
+     */
+    public function __construct(SymfonyApplication $application = null)
+    {
+        $this->application = $application ?: new SymfonyApplication();
+        $this->addCommandsToApplication();
+    }
+
+    /**
+     * Run the application
+     *
+     * @throws \Exception
+     */
+    public function run()
+    {
+        $this->application->run();
+    }
+
+    /**
+     * @return $this
+     */
+    protected function addCommandsToApplication()
+    {
+        $this->application->add(new GenerateCommand());
+        $this->application->add(new AdminRoute());
+        $this->application->add(new FrontendRoute());
+        $this->application->add(new FrontendTemplateBlock());
+        $this->application->add(new AdminTemplateBlock());
+
+        return $this;
+    }
+}
